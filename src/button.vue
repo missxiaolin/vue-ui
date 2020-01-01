37c6fd7e64e8145dca7e1fb68bf81d25974c65d1
xiaolin <462441355@qq.com>
2020-01-01 17:01:43 +0800

    * 按钮增加图标

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,29 @@
 <template>
-    <button class="g-button">按钮</button>
+    <div>
+        <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+            <svg v-if="icon" class="icon">
+                <use :xlink:href="`#icon-${icon}`"></use>
+            </svg>
+            <div class="g-button-content">
+                <slot></slot>
+            </div>
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'GuluButton',
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator (value) {
+                return value === 'left' || value === 'right'
+            }
+        }
+    }
 }
 </script>
 
@@ -15,6 +35,9 @@ export default {
     border-radius: var(--button-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
     &:hover {
         border-color: var(--border-color-hover);
     }
@@ -24,7 +47,26 @@ export default {
     &:focus {
         outline: none;
     }
-    
+    > .g-button-content {
+        order: 2;
+    }
+    > .icon {
+        width: 1em;
+        height: 1em;
+        vertical-align: middle;
+        order: 1;
+        margin-right: .1em;
+    }
+    &.icon-right {
+      > .g-button-content {
+          order: 1; 
+        }
+      > .icon {
+          order: 2;
+          margin-right: 0;
+          margin-left: .1em;
+        }
+    } 
 }
 
 </style>
