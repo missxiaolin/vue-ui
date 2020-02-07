4023f4cfdb5fd726fc1db92e00656fe652466cd2
xiaolin <462441355@qq.com>
2020-02-07 22:01:25 +0800

    * 修改成局部样式 scoped

--- src/button.vue
+++ src/button.vue
@@ -1,89 +1,88 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
-        <g-icon class="icon" v-if="!loding && icon" :name="icon"></g-icon>
-        <g-icon v-if="loding" class="loding icon"  name="jiazaizhong"></g-icon>
-        <div class="g-button-content">
-            <slot></slot>
-        </div>
-    </button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="!loding && icon" :name="icon"></g-icon>
+    <g-icon v-if="loding" class="loding icon" name="jiazaizhong"></g-icon>
+    <div class="g-button-content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-import Icon from './icon'
+import Icon from "./icon";
 
 export default {
-    name: 'L-Button',
-    components: {
-        'g-icon': Icon
+  name: "L-Button",
+  components: {
+    "g-icon": Icon
+  },
+  props: {
+    icon: {},
+    loding: {
+      type: Boolean,
+      default: false
     },
-    props: {
-        icon: {},
-        loding: {
-            type: Boolean,
-            default: false
-        },
-        iconPosition: {
-            type: String,
-            default: 'left',
-            validator (value) {
-                return value === 'left' || value === 'right'
-            }
-        }
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      }
     }
-}
+  }
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @keyframes spin {
-    0%{
-        transform: rotate(0deg);
-    }
-    100%{
-        transform: rotate(360deg);
-    }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--button-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    &:hover {
-        border-color: var(--border-color-hover);
-    }
-    &:active {
-        background-color: var(--button-active-bg);
-    }
-    &:focus {
-        outline: none;
-    }
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--button-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .g-button-content {
+    order: 2;
+  }
+  > .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: middle;
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
     > .g-button-content {
-        order: 2;
+      order: 1;
     }
     > .icon {
-        width: 1em;
-        height: 1em;
-        vertical-align: middle;
-        order: 1;
-        margin-right: .1em;
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
     }
-    &.icon-right {
-      > .g-button-content {
-          order: 1; 
-        }
-      > .icon {
-          order: 2;
-          margin-right: 0;
-          margin-left: .1em;
-        }
-    } 
-    .loding {
-        animation: spin 1s infinite linear;
-    }
+  }
+  .loding {
+    animation: spin 1s infinite linear;
+  }
 }
-
 </style>
--- src/icon.vue
+++ src/icon.vue
@@ -1,16 +1,18 @@
 <template>
   <svg class="g-icon" @click="$emit('click', $event)">
-    <use :xlink:href="`#icon-${name}`"></use>
+    <use :xlink:href="`#icon-${name}`" />
   </svg>
 </template>
 <script>
-
-  import './svg.js'
-  export default {
-    name: 'L-Icon',
-    props: ['name']
-  }
+import "./svg.js";
+export default {
+  name: "L-Icon",
+  props: ["name"]
+};
 </script>
 <style lang="scss" scoped>
-  .g-icon {width: 1em; height: 1em; }
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
 </style>
