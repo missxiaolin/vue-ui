26ba7d9208b6ce5cad386a53b967a37487d569f1
xiaolin <462441355@qq.com>
2021-11-26 14:56:57 +0800

    * 添加single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
-      {{title}}
+    <div class="title" @click="toggle">
+      {{ title }}
     </div>
     <div class="content" v-if="open">
       <slot></slot>
@@ -10,50 +10,72 @@
 </template>
 
 <script>
-  export default {
-    name: "LCollapseItem",
-    props: {
-      title: {
-        type: String,
-        required: true
+export default {
+  name: "LCollapseItem",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+  inject: ["eventBus"],
+  mounted() {
+    this.eventBus &&
+      this.eventBus.$on("update:selected", (vm) => {
+        if (vm !== this) {
+          this.close();
+        }
+      });
+  },
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.open = false;
+      } else {
+        this.open = true;
+        this.eventBus && this.eventBus.$emit("update:selected", this);
       }
     },
-    data () {
-      return {
-        open: false
-      }
-    }
-  }
+    close() {
+      this.open = false;
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  $grey: #ddd;
-  $border-radius: 4px;
-  .collapseItem {
+$grey: #ddd;
+$border-radius: 4px;
+.collapseItem {
+  > .title {
+    border: 1px solid $grey;
+    margin-top: -1px;
+    margin-left: -1px;
+    margin-right: -1px;
+    min-height: 32px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
+  }
+  &:first-child {
     > .title {
-      border: 1px solid $grey;
-      margin-top: -1px;
-      margin-left: -1px;
-      margin-right: -1px;
-      min-height: 32px;
-      display: flex;
-      align-items: center;
-      padding: 0 8px;
-    }
-    &:first-child {
-      > .title {
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
-    }
-    &:last-child {
-      > .title:last-child {
-        border-bottom-left-radius: $border-radius;
-        border-bottom-right-radius: $border-radius;
-      }
-    }
-    > .content {
-      padding: 8px;
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
     }
   }
+  &:last-child {
+    > .title:last-child {
+      border-bottom-left-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
+    }
+  }
+  > .content {
+    padding: 8px;
+  }
+}
 </style>
