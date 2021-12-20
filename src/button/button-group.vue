79087b57adebd273d3ee39d6ccdfc1ce106e8396
xiaolin <462441355@qq.com>
2021-12-20 15:51:07 +0800

    * 测试用例

--- src/button/button-group.vue
+++ src/button/button-group.vue
@@ -11,7 +11,7 @@ export default {
       let name = node.nodeName.toLowerCase();
       if (name !== "button") {
         console.warn(
-          `g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`
+          `l-button-group 的子元素应该全是 l-button，但是你写的是 ${name}`
         );
       }
     }
