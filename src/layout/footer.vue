e99b1dd43eaeabc2e67b559fcc8e877d748450fd
xiaolin <462441355@qq.com>
2021-12-29 10:26:37 +0800

    * 导出增加name

--- src/layout/footer.vue
+++ src/layout/footer.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'L-Footer'
+}
+</script>
+
 <style lang="scss" scoped>
 .footer {
 }
--- src/layout/header.vue
+++ src/layout/header.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'L-Header'
+}
+</script>
+
 <style lang="scss" scoped>
 .header {
 }
